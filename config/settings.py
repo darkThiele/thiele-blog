2ae6d642d984d539542ccce50497e30e1e8c5881
Hatsunori <hatsunri.kashima@gmail.com>
2019-09-01 12:50:39 +0900

    Spell correct

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LYFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
 }
 
 # CORS
